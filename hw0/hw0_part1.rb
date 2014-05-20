8f56e918338ba9b2859fc9dc20d1a7b4ea66ba1b
Albert Yang <ayoungbest@gmail.com>
2014-05-20 03:55:39 -0400

    hw0 added comment

--- hw0/hw0_part1.rb
+++ hw0/hw0_part1.rb
@@ -1,8 +1,10 @@
+# returns the sum of an integer array
 def sum (array) 
 	return 0 if array.empty?
 	array.inject(:+)
 end
 
+# returns the sum of the two largest elements in an integer array
 def max_2_sum (array) 
 	return 0 if array.empty?
 	return array[0] if array.size == 1
@@ -10,6 +12,7 @@ def max_2_sum (array)
 	return array[array.size - 1] + array[array.size - 2]
 end
 
+# determines if any two distinct elements in the array of integers sum to n
 def sum_to_n? (array, n)
 	return false if array.empty? or array.size == 1
 	return array.combination(2).any? {|a,b| a + b == n}
