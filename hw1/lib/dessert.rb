7f58bcf8092a6db6b06e6b7691bafd4939c76c89
Albert Yang <ayoungbest@gmail.com>
2014-05-20 21:55:12 -0400

    desert

--- hw1/lib/dessert.rb
+++ hw1/lib/dessert.rb
@@ -1,17 +1,32 @@
 class Dessert
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
     # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
     # your code here
+    @calories < 200 
   end
   def delicious?
     # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
+
   def initialize(flavor)
     # your code here
+    @calories = 5
+    @flavor = flavor
+    @name = @flavor + " jelly bean"
   end
+  def delicious?
+    @flavor == "licorice"? false : true
+  end
+
 end
