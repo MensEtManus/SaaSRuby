b3428ea9ddebae445ed39eb185ea9fcb159d50fd
Albert Yang <ayoungbest@gmail.com>
2014-06-02 17:28:38 -0400

    updated hw1

--- hw1/lib/dessert.rb
+++ hw1/lib/dessert.rb
@@ -2,13 +2,16 @@ class Dessert
   attr_accessor :name, :calories
 
   def initialize(name, calories)
+    # your code here
     @name = name
     @calories = calories
   end
   def healthy?
+    # your code here
     @calories < 200 
   end
   def delicious?
+    # your code here
     return true
   end
 end
@@ -17,6 +20,7 @@ class JellyBean < Dessert
   attr_accessor :flavor
 
   def initialize(flavor)
+    # your code here
     @calories = 5
     @flavor = flavor
     @name = @flavor + " jelly bean"
--- hw1/lib/rock_paper_scissors.rb
+++ hw1/lib/rock_paper_scissors.rb
@@ -4,11 +4,39 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    if /[RPS]/.match(player1[1]) == nil or /[RPS]/.match(player2[1]) == nil
+    	raise NoSuchStrategyError, "Strategy must be one of R,P,S" 	
+    end
+    return player1 if player1[1] == player2[1]
+    if player1[1] == 'R' 
+    	if player2[1] == 'S' 
+    		return player1
+    	else
+    		return player2
+    	end
+    elsif player1[1] == 'P'
+    	if player2[1] == 'S'
+    		return player2
+    	else
+    		return player1
+    	end
+    elsif player1[1] == 'S'
+    	if player2[1] == 'P'
+    		return player1
+    	else
+    		return player2
+    	end
+    end
+    	
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+  	if tournament[0][1].class == String
+  		return winner(tournament[0], tournament[1])
+  	else
+  		winner(tournament_winner(tournament[0]), tournament_winner(tournament[1]))
+  	end
+	    
   end
 
 end
